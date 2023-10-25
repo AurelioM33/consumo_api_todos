a8b106de8d5abec77343a7741940447e927b7949
AurelioM33 <m.aurelio887@gmail.com>
2023-10-25 13:00:32 -0300

    main/home/add_page

---
+++ lib/add_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AddTodoPage extends StatefulWidget {
+  const AddTodoPage({super.key});
+
+  @override
+  State<AddTodoPage> createState() => _AddTodoPageState();
+}
+
+class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Todo'),
+      ),
+      body: ListView(
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: const InputDecoration(hintText: 'Title'),
+          ),
+          TextField(
+            controller: descriptionController,
+            decoration: const InputDecoration(hintText: 'Description'),
+            keyboardType: TextInputType.multiline,
+            minLines: 5,
+            maxLines: 8,
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: submitData,
+            child: const Text('Submit'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> submitData() async {
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+    };
+
+    const url = 'http://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+
+    final response = await http.post(uri, body: body);
+
+    // ignore: avoid_print
+    print(response);
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:consumo_api/add_page.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: navigateToAddPage,
+        label: const Text('Add'),
+      ),
+      appBar: AppBar(
+        title: const Text(
+          "Todo List",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void navigateToAddPage() {
+    final route = MaterialPageRoute(
+      builder: (context) => const AddTodoPage(),
+    );
+    Navigator.push(context, route);
+  }
+}
