04a35f051dee02b581ac7bc17f2e86f2b4471a4a
AurelioM33 <m.aurelio887@gmail.com>
2023-11-01 22:04:02 -0300

    fontbold_colorgrey_fontsize

--- lib/add_page.dart
+++ lib/add_page.dart
@@ -32,10 +32,10 @@ class _AddTodoPageState extends State<AddTodoPage> {
             controller: descriptionController,
             decoration: const InputDecoration(hintText: 'Description'),
             keyboardType: TextInputType.multiline,
-            minLines: 6,
-            maxLines: 9,
+            minLines: 5,
+            maxLines: 8,
           ),
-          const SizedBox(height: 20),
+          const SizedBox(height: 22),
           ElevatedButton(
             onPressed: submitData,
             child: const Text('Submit'),
